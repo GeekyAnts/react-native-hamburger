6bbd490a6d13825656dd350ef24fae4fde0d50fe
hmoule <57354264+hmoule@users.noreply.github.com>
2020-05-20 20:10:17 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,10 @@
-export default interface HamburgerProps {
+import { Component } from "react";
+
+interface HamburgerProps {
   active: boolean;
   type: 'cross' | 'spinCross' | 'arrow' | 'spinArrow';
   color?: string;
   onPress?: () => void;
 }
+
+export default class Hamburger extends Component<HamburgerProps> {}
